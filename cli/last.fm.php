d0bab4b0df20425d80517ccf3141a21b9fb19eef
Julio Foulquié <julio@tnwlabs.com>
2015-04-18 17:53:28 +0200

    WIP: first version of last.fm web forums scrapper.

--- cli/last.fm.php
+++ cli/last.fm.php
@@ -1,2 +1,25 @@
 <?php
 
+/**
+ *  Scrape the last articles from the last.fm forum
+ */
+
+// Find all article blocks
+$authors_names = array();
+foreach($html->find('table tr') as $post) {
+	if(!empty($post) && !empty($post->find('.lastPost a', 0))) {
+		$authors_names[] = $post->find('.threadAuthor a', 0)->plaintext;
+	}
+}
+
+$authors = array();
+foreach($authors_names AS $author) {
+	$url_auth = 'http://www.last.fm/user/' .$author;
+	$html_profile = \SimpleHtmlDom\file_get_html($url_auth);
+// 	$staff = $html_profile->find('#content div.badgeAvatar', 0)->plaintext;
+	$staff = !empty($html_profile->find('#content div.badgeAvatar', 0)) ? trim($html_profile->find('#content div.badgeAvatar', 0)->plaintext) : FALSE;
+// 	$staff = !empty(trim($html_profile->find('#content div.badgeAvatar', 0))) ? TRUE : FALSE;
+	$authors[$author] = empty($staff) ? FALSE : TRUE;
+}
+
+var_dump($authors);
