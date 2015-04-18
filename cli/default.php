68781794c194290e18a6c3b7d0ad5cb3bffd1ea1
Julio Foulquié <julio@tnwlabs.com>
2015-04-18 23:57:54 +0200

    Better handle empty response on the default scrapper.

--- cli/default.php
+++ cli/default.php
@@ -1,5 +1,8 @@
 <?php
 
 $log->addDebug("{$url_parts['host']} not found. Default.");
-
-$log->addInfo("Title of the page is: " . html_entity_decode($html->find("head title", 0)->plaintext));
+if(!empty($html)) {
+	$log->addInfo("Title of the page is: " . html_entity_decode($html->find("head title", 0)->plaintext));
+} else {
+	$log->addInfo("Empty response");
+}
