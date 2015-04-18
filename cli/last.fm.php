a3bea7ddbd34d44b6a9dbc005af9128a4f28733e
Julio Foulquié <julio@tnwlabs.com>
2015-04-18 19:32:42 +0200

    Finish last.fm web forum parser.

--- cli/last.fm.php
+++ cli/last.fm.php
@@ -6,20 +6,27 @@
 
 // Find all article blocks
 $authors_names = array();
+$dates = array();
 foreach($html->find('table tr') as $post) {
 	if(!empty($post) && !empty($post->find('.lastPost a', 0))) {
-		$authors_names[] = $post->find('.threadAuthor a', 0)->plaintext;
+		$authors_names[] = $post->find('.lastPost a', 0)->plaintext;
+		$dates[] = $post->find('.lastPost small a', 0)->plaintext;
 	}
 }
 
+$log->addInfo(count($authors_names) . " posts found, retrieving authors info.");
+
 $authors = array();
-foreach($authors_names AS $author) {
+foreach($authors_names AS $index => $author) {
 	$url_auth = 'http://www.last.fm/user/' .$author;
-	$html_profile = \SimpleHtmlDom\file_get_html($url_auth);
-// 	$staff = $html_profile->find('#content div.badgeAvatar', 0)->plaintext;
-	$staff = !empty($html_profile->find('#content div.badgeAvatar', 0)) ? trim($html_profile->find('#content div.badgeAvatar', 0)->plaintext) : FALSE;
-// 	$staff = !empty(trim($html_profile->find('#content div.badgeAvatar', 0))) ? TRUE : FALSE;
-	$authors[$author] = empty($staff) ? FALSE : TRUE;
+	$html_profile = @\SimpleHtmlDom\file_get_html($url_auth);	// The function throws PHP warnings on HTTP errors like 404 or 401, silence those.
+	if(!empty($html_profile)) {
+		$staff = !empty(trim($html_profile->find('#content div.badgeAvatar', 0))) ? trim($html_profile->find('#content div.badgeAvatar', 0)->plaintext) : 'false_prof';
+		$staff = (empty($staff) OR $staff == "subscriber") ? 'false' : $staff;
+		$authors[$author] = array('role' => $staff, 'post_date' => $dates[$index]);
+	} else {
+		$authors[$author] = array('role' => 'user does not exist', 'post_date' => $dates[$index]);
+	}
 }
 
-var_dump($authors);
+print_r($authors);
